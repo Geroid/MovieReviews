68680cd7bc9e58bf46d7c0d2133d4badc6f9f8f8
Viktor Rezvantsev <geroid1735@gmail.com>
2020-07-24 18:52:18 +0300

    Initial Commit

---
+++ MovieReviews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieReviews
+//
+//  Created by Viktor Rezvantsev on 24.07.2020.
+//  Copyright © 2020 Viktor Rezvantsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
